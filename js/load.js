a0bb31986d83ff6c2a745152c88c15ba13c3a555
James Scott-Brown <james@jamesscottbrown.com>
2018-05-29 16:58:45 +0100

    add video link beneath other links to right of paper details

--- js/load.js
+++ js/load.js
@@ -10,7 +10,7 @@ var dayAbrevs = {"Saturday morning": "Sa AM", "Saturday afternoon": "Sa PM",
                  "Thursday morning": "Th AM", "Thursday afternoon": "Th PM", 
                  "Friday morning": "F AM", "Friday afternoon": "F PM"};
 var OADomains = ["osf.io", "arxiv.org", "biorxiv.org", "psyarxiv.org", "/hal.", "/eprints.", "/openaccess."];
-var linkImages = {"PDF": "file-text", "Materials": "materials", "Data": "data", "Explanation": "info"};
+var linkImages = {"PDF": "file-text", "Materials": "materials", "Data": "data", "Explanation": "info", "Video": "video"};
 var timeParser = d3.timeParse("%I:%M %p");
 var style = "col-md-12 col-lg-10 col-lg-offset-1";
 var untouched = true;
@@ -132,6 +132,7 @@ function buildPage() {
   right.appendLink("sourceMaterial", "Materials", d => d.SourceMaterials);
   right.appendLink("data", "Data", d => d.Data);
   right.appendLink("projectPage", "Explanation", d => d.ExplanationPage);
+  right.appendLink("video", "Video", d => d.Video);
 
 
   ///// expander content  /////
